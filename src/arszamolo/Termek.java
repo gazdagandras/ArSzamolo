191e332dc3a3bfaabee8c2372ee35d7932ea45d3
Andras Gazdag <gazdag.andras@gmail.com>
2013-11-17 22:56:21 +0100

    Termek osztály, kódrészletek és kommentek

---
+++ src/arszamolo/Termek.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arszamolo;
+
+/**
+ *
+ * @author Gazdag András
+ */
+public class Termek {
+    private String termekNev;
+    private int beszerzesiAr;
+    
+    public Termek(String nev, int ar) {
+        termekNev = nev;
+        beszerzesiAr = ar;
+    }
+    
+    public int getBeszerzesiAr() {
+        return beszerzesiAr;
+    }
+    
+    public String getTermekNev() {
+        return termekNev;
+    }
+}
